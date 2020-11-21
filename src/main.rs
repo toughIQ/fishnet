50d8d66136ab563f74abfe523d143c7160ed3afb
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-11-21 13:18:27 +0100

    some progress on application structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Package name reserved for next version of https://github.com/niklasf/fishnet");
+use std::time::Duration;
+use tokio::{signal, time, sync};
+
+#[derive(Debug)]
+struct Product {
+    res: (),
+    next_tx: sync::oneshot::Sender<()>,
+}
+
+/// Produces analysis.
+async fn producer(tx: sync::mpsc::Sender<Product>) {
+    loop {
+        println!("working ...");
+        time::sleep(Duration::from_millis(5000)).await;
+
+        let (next_tx, next_rx) = sync::oneshot::channel();
+
+        tx.send(Product {
+            res: (),
+            next_tx,
+        }).await.expect("send");
+
+        let job = next_rx.await.expect("main sender not dropped");
+        dbg!(job);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let num_threads = 2;
+
+    let (tx, mut rx) = sync::mpsc::channel(num_threads);
+
+    for _ in 0..2 {
+        let tx = tx.clone();
+        tokio::spawn(async move {
+            producer(tx).await;
+        });
+    }
+
+    while let Some(req) = rx.recv().await {
+        dbg!(req).next_tx.send(()).expect("send");
+    }
 }
