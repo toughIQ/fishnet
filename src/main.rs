c5b1c347b3852869053ba276a768aa2126a9c649
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-11-21 15:40:03 +0100

    clean shutdown procedure

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,72 @@
 use std::time::Duration;
 use tokio::signal::unix::SignalKind;
-use tokio::{signal, time, sync};
+use tokio::{signal, time, sync, process};
+
+#[derive(Debug)]
+enum Job {
+    Analysis(AnalysisJob),
+    Idle,
+    Shutdown,
+}
+
+#[derive(Debug)]
+struct AnalysisJob;
+
+#[derive(Debug)]
+struct AnalysisResult;
 
 #[derive(Debug)]
 struct Product {
-    res: (),
-    next_tx: sync::oneshot::Sender<()>,
+    res: Option<AnalysisResult>,
+    next_tx: sync::oneshot::Sender<Job>,
 }
 
 /// Produces analysis.
 async fn producer(id: usize, tx: sync::mpsc::Sender<Product>) {
+    //let mut child = process::Command::new("stockfish")
+    //    .spawn()
+    //    .expect("start stockfish");
+
+    let mut job: Option<AnalysisJob> = None;
+
+    let prefix = " ".repeat(id * 15);
+
     loop {
-        println!("{} working ...", " ".repeat(id * 15));
-        time::sleep(Duration::from_millis(5000)).await;
-        println!("{} ... worked.", " ".repeat(id * 15));
+        let job_result = if let Some(job) = job.take() {
+            println!("{} working ({:?}) ...", prefix, job);
+            time::sleep(Duration::from_millis(5000)).await;
+            println!("{} ... worked.", prefix);
+            Some(AnalysisResult)
+        } else {
+            None
+        };
 
         let (next_tx, next_rx) = sync::oneshot::channel();
 
         tx.send(Product {
-            res: (),
+            res: job_result,
             next_tx,
         }).await.expect("send");
 
-        let _job = next_rx.await.expect("main sender not dropped");
+        match next_rx.await {
+            Ok(Job::Analysis(ana)) => {
+                job = Some(ana);
+            }
+            Ok(Job::Idle) => {
+                println!("{} idling ...", prefix);
+            }
+            Ok(Job::Shutdown) => {
+                println!("{} shutting down", prefix);
+                break;
+            }
+            Err(_) => {
+                println!("{} next_tx dropped", prefix);
+            }
+        }
     }
+
+    time::sleep(Duration::from_millis(2000)).await;
+    println!("{} shut down", prefix);
 }
 
 #[tokio::main]
@@ -38,30 +81,50 @@ async fn main() {
             producer(id, tx).await;
         });
     }
+    drop(tx);
 
     let mut in_queue: usize = 0;
 
     let mut ctrl_c = signal::unix::signal(SignalKind::interrupt()).expect("install signal handler");
 
+    let mut shutdown_soon = false;
+
     loop {
         tokio::select! {
             req = rx.recv() => {
                 if let Some(req) = req {
-                    if in_queue == 0 {
-                        println!("fetching ...");
-                        time::sleep(Duration::from_millis(2000)).await;
-                        println!("... fetched.");
-                        in_queue += 7;
+                    if let Some(res) = req.res {
+                        println!("got result");
                     }
 
-                    in_queue -= 1;
-                    req.next_tx.send(()).expect("send");
+                    if in_queue == 0 {
+                        if shutdown_soon {
+                            println!("fetching no more!");
+                        } else {
+                            println!("fetching ...");
+                            time::sleep(Duration::from_millis(2000)).await;
+                            println!("... fetched.");
+                            in_queue += 7;
+                        }
+                    }
+
+                    if in_queue > 0 {
+                        in_queue -= 1;
+                        req.next_tx.send(Job::Analysis(AnalysisJob)).expect("send to worker");
+                    } else if shutdown_soon {
+                        req.next_tx.send(Job::Shutdown).expect("send to worker");
+                    } else {
+                        req.next_tx.send(Job::Idle).expect("send to worker");
+                    }
+                } else {
+                    println!("all workers exited");
+                    return;
                 }
             }
             res = ctrl_c.recv() => {
                 res.expect("signal handler installed");
                 println!("ctrl+c");
-                return;
+                shutdown_soon = true;
             }
         }
     }
