7397b024143c0332a88c28754697010a7dd22a44
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-12-01 13:56:15 +0100

    update to rustls 0.20.x

--- src/api.rs
+++ src/api.rs
@@ -537,10 +537,19 @@ impl ApiActor {
         logger: Logger,
     ) -> ApiActor {
         // Build TLS backend that supports SSLKEYLOGFILE.
-        let mut tls = rustls::ClientConfig::new();
-        tls.set_protocols(&["h2".into(), "http/1.1".into()]);
-        tls.root_store
-            .add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+        let mut root_store = rustls::RootCertStore::empty();
+        root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+            rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
+                ta.subject,
+                ta.spki,
+                ta.name_constraints,
+            )
+        }));
+        let mut tls = rustls::ClientConfig::builder()
+            .with_safe_defaults()
+            .with_root_certificates(root_store)
+            .with_no_client_auth();
+        tls.alpn_protocols = vec!["h2".into(), "http/1.1".into()];
         tls.key_log = Arc::new(rustls::KeyLogFile::new());
 
         ApiActor {
