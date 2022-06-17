8b65ba4f03580048c069d8e296a66cdd9695196c
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-06-17 11:21:59 +0200

    deduplicate self-invocation generation

--- src/systemd.rs
+++ src/systemd.rs
@@ -1,4 +1,5 @@
 use std::{env, fs};
+use std::path::{Path, PathBuf};
 
 use atty::Stream;
 use shell_escape::escape;
@@ -6,14 +7,13 @@ use shell_escape::escape;
 use crate::configure::{Key, Opt};
 
 pub fn systemd_system(opt: Opt) {
-    let exe = exec_start(&opt);
     println!("[Unit]");
     println!("Description=Fishnet client");
     println!("After=network-online.target");
     println!("Wants=network-online.target");
     println!();
     println!("[Service]");
-    println!("ExecStart={} run", exe);
+    println!("ExecStart={} run", exec_start(Invocation::Absolute, &opt));
     println!("KillMode=mixed");
     println!("WorkingDirectory=/tmp");
     println!(
@@ -25,7 +25,7 @@ pub fn systemd_system(opt: Opt) {
     println!("PrivateTmp=true");
     println!("PrivateDevices=true");
     println!("DevicePolicy=closed");
-    if opt.auto_update && exe.starts_with("/usr/") {
+    if opt.auto_update && env::current_exe().expect("current exe").starts_with("/usr/") {
         println!("ProtectSystem=false");
     } else {
         println!("ProtectSystem=full");
@@ -37,34 +37,33 @@ pub fn systemd_system(opt: Opt) {
     println!("WantedBy=multi-user.target");
 
     if atty::is(Stream::Stdout) {
+        let command = exec_start(Invocation::Relative, &opt);
         eprintln!();
         eprintln!("# Example usage:");
         eprintln!(
-            "# {} systemd | sudo tee /etc/systemd/system/fishnet.service",
-            shell_start(&opt)
+            "# {} systemd | sudo tee /etc/systemd/system/fishnet.service", command
         );
         eprintln!("# systemctl enable fishnet.service");
         eprintln!("# systemctl start fishnet.service");
         eprintln!("# Live view of log: journalctl --unit fishnet --follow");
-        eprintln!("# Prefer a user unit? {} systemd-user", shell_start(&opt));
+        eprintln!("# Prefer a user unit? {} systemd-user", command);
     }
 }
 
 pub fn systemd_user(opt: Opt) {
-    let exe = exec_start(&opt);
     println!("[Unit]");
     println!("Description=Fishnet client");
     println!("After=network-online.target");
     println!("Wants=network-online.target");
     println!();
     println!("[Service]");
-    println!("ExecStart={} run", exe);
+    println!("ExecStart={} run", exec_start(Invocation::Absolute, &opt));
     println!("KillMode=mixed");
     println!("WorkingDirectory=/tmp");
     println!("Nice=5");
     println!("PrivateTmp=true");
     println!("DevicePolicy=closed");
-    if opt.auto_update && exe.starts_with("/usr/") {
+    if opt.auto_update && env::current_exe().expect("current exe").starts_with("/usr/") {
         println!("ProtectSystem=false");
     } else {
         println!("ProtectSystem=full");
@@ -79,7 +78,7 @@ pub fn systemd_user(opt: Opt) {
         eprintln!("# Example usage:");
         eprintln!(
             "# {} systemd-user | tee ~/.config/systemd/user/fishnet.service",
-            shell_start(&opt)
+            exec_start(Invocation::Relative, &opt)
         );
         eprintln!("# systemctl enable --user fishnet.service");
         eprintln!("# systemctl start --user fishnet.service");
@@ -87,13 +86,30 @@ pub fn systemd_user(opt: Opt) {
     }
 }
 
-fn exec_start(opt: &Opt) -> String {
-    let exe = env::current_exe()
-        .expect("current exe")
-        .to_str()
-        .expect("printable exec path")
-        .to_owned();
-    let mut builder = vec![escape(exe.into()).into_owned()];
+#[derive(Copy, Clone, Eq, PartialEq)]
+enum Invocation {
+    Absolute,
+    Relative,
+}
+
+impl Invocation {
+    fn exe(self) -> PathBuf {
+        match self {
+            Invocation::Absolute => env::current_exe().expect("current exe"),
+            Invocation::Relative => env::args().next().expect("argv[0]").into(),
+        }
+    }
+
+    fn path<P: AsRef<Path>>(self, path: P) -> PathBuf {
+        match self {
+            Invocation::Absolute => fs::canonicalize(path).expect("canonicalize path"),
+            Invocation::Relative => path.as_ref().into(),
+        }
+    }
+}
+
+fn exec_start(invocation: Invocation, opt: &Opt) -> String {
+    let mut builder = vec![escape(invocation.exe().to_str().expect("printable exe path").into()).into_owned()];
 
     if opt.verbose.level > 0 {
         builder.push(format!("-{}", "v".repeat(usize::from(opt.verbose.level))));
@@ -104,81 +120,19 @@ fn exec_start(opt: &Opt) -> String {
 
     if opt.no_conf {
         builder.push("--no-conf".to_owned());
-    } else {
+    } else if opt.conf.is_some() || invocation == Invocation::Absolute {
         builder.push("--conf".to_owned());
-        let canonical = fs::canonicalize(opt.conf())
-            .expect("canonicalize config path")
-            .to_str()
-            .expect("printable config path")
-            .to_owned();
-        builder.push(escape(canonical.into()).into_owned());
+        builder.push(escape(invocation.path(opt.conf()).to_str().expect("printable --conf path").into()).into_owned());
     }
 
     if let Some(ref key_file) = opt.key_file {
         builder.push("--key-file".to_owned());
-        let canonical = fs::canonicalize(key_file)
-            .expect("canonicalize key file path")
-            .to_str()
-            .expect("printable key file path")
-            .to_owned();
-        builder.push(escape(canonical.into()).into_owned());
+        builder.push(escape(invocation.path(key_file).to_str().expect("printable --key-file path").into()).into_owned());
     } else if let Some(Key(ref key)) = opt.key {
         builder.push("--key".to_owned());
         builder.push(escape(key.into()).into_owned());
     }
 
-    if let Some(ref endpoint) = opt.endpoint {
-        builder.push("--endpoint".to_owned());
-        builder.push(escape(endpoint.to_string().into()).into_owned());
-    }
-    if let Some(ref cores) = opt.cores {
-        builder.push("--cores".to_owned());
-        builder.push(escape(cores.to_string().into()).into_owned());
-    }
-    if let Some(ref max_backoff) = opt.max_backoff {
-        builder.push("--max-backoff".to_owned());
-        builder.push(max_backoff.to_string());
-    }
-    if let Some(ref user_backlog) = opt.backlog.user {
-        builder.push("--user-backlog".to_owned());
-        builder.push(escape(user_backlog.to_string().into()).into_owned());
-    }
-    if let Some(ref system_backlog) = opt.backlog.system {
-        builder.push("--system-backlog".to_owned());
-        builder.push(escape(system_backlog.to_string().into()).into_owned());
-    }
-
-    builder.join(" ")
-}
-
-fn shell_start(opt: &Opt) -> String {
-    let exe = env::args()
-        .next()
-        .unwrap_or_else(|| "./fishnet".to_string());
-    let mut builder = vec![escape(exe.into()).into_owned()];
-
-    if opt.verbose.level > 0 {
-        builder.push(format!("-{}", "v".repeat(usize::from(opt.verbose.level))));
-    }
-    if opt.auto_update {
-        builder.push("--auto-update".to_owned());
-    }
-    if let Some(ref conf) = opt.conf {
-        builder.push("--conf".to_owned());
-        builder.push(escape(conf.to_str().expect("printable config path").into()).into_owned());
-    }
-    if opt.no_conf {
-        builder.push("--no-conf".to_owned());
-    }
-    if let Some(Key(ref key)) = opt.key {
-        builder.push("--key".to_owned());
-        builder.push(escape(key.into()).into_owned());
-    }
-    if let Some(ref key_file) = opt.key_file {
-        builder.push("--key-file".to_owned());
-        builder
-            .push(escape(key_file.to_str().expect("printable key file path").into()).into_owned());
-    }
     if let Some(ref endpoint) = opt.endpoint {
         builder.push("--endpoint".to_owned());
         builder.push(escape(endpoint.to_string().into()).into_owned());
