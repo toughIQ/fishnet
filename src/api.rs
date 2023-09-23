2102b272f6db304ddcc0a72acb7621b589cee209
Niklas Fiekas <niklas.fiekas@backscattering.de>
2023-09-23 13:48:55 +0200

    update reqwest and rustls

--- src/api.rs
+++ src/api.rs
@@ -487,7 +487,7 @@ impl ApiActor {
     ) -> ApiActor {
         // Build TLS backend that supports SSLKEYLOGFILE.
         let mut root_store = rustls::RootCertStore::empty();
-        root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
+        root_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
             rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
                 ta.subject,
                 ta.spki,
